989583d6de5fb56cdcc1340a730ff90f9a7831f5
Sreenayak <sreenayak711@gmail.com>
2026-08-19 14:06:15 +0530

    Updated the quick sort code.

--- practical_1/Quick_sort/Quick_sort.c
+++ practical_1/Quick_sort/Quick_sort.c
@@ -1,81 +1,59 @@
 #include <stdio.h>
 
-
-void swap(int *a, int *b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-
 int partition(int arr[], int low, int high)
 {
     int pivot = arr[high];
     int i = low - 1;
-    int j;
+    int j, temp;
 
     for (j = low; j < high; j++)
     {
         if (arr[j] < pivot)
         {
             i++;
-            swap(&arr[i], &arr[j]);
+
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
     }
 
-
-    swap(&arr[i + 1], &arr[high]);
+    temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
 
     return i + 1;
 }
-
 
 void quickSort(int arr[], int low, int high)
 {
     if (low < high)
     {
-    
-        int pivotIndex = partition(arr, low, high);
+        int p = partition(arr, low, high);
 
-        
-        quickSort(arr, low, pivotIndex - 1);
-
-        
-        quickSort(arr, pivotIndex + 1, high);
+        quickSort(arr, low, p - 1);
+        quickSort(arr, p + 1, high);
     }
 }
 
 int main()
 {
+    int arr[100];
     int n, i;
 
-    printf("Enter the number of elements: ");
+    printf("Enter number of elements: ");
     scanf("%d", &n);
 
-    int arr[n];
-
-
-    printf("Enter %d elements:\n", n);
+    printf("Enter elements:\n");
 
     for (i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
 
-    // Display original array
-    printf("\nOriginal array: ");
-
-    for (i = 0; i < n; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-
-
     quickSort(arr, 0, n - 1);
 
-    // Display sorted array
-    printf("\nSorted array: ");
+    printf("Sorted array: ");
 
     for (i = 0; i < n; i++)
     {
